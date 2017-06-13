b868c892da45d2e2930b9263d85682ee14b1e5a5
Jeehoon Cha <j.cha@campus.tu-berlin.de>
2017-06-13 21:42:57 +0900

    first implementation of training network

--- dqn/history.py
+++ dqn/history.py
@@ -7,12 +7,16 @@ class History:
 
     batch_size, history_length, screen_height, screen_width = \
         config.batch_size, config.history_length, config.screen_height, config.screen_width
+
+    self.screen_height = screen_height
+    self.screen_width = screen_width
+
     self.history = np.zeros([history_length, screen_height, screen_width], dtype=np.int32)
 
   def add(self, screen):
     # resize screen image size added by haeyong
     screen = np.array(screen, dtype=np.uint8)
-    screen = cv2.resize(screen, (self.screen_height,self.screen_width), interpolation=cv2.INTER_CUBIC)
+    screen = cv2.resize(screen, (self.screen_height ,self.screen_width), interpolation=cv2.INTER_CUBIC)
 
     self.history[:-1] = self.history[1:]
     self.history[-1] = screen
